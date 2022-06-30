8aa92fe237630e32bc1a0ea91ee91ff72b560e37
Alexander Nusov <nusov@users.noreply.github.com>
2022-06-30 12:22:16 +0300

    Update LuaOverMQ.cpp

--- LuaOverMQ.cpp
+++ LuaOverMQ.cpp
@@ -25,28 +25,28 @@ constexpr auto STATUS_OK = "OK";
 // String Utils
 std::string wide_to_ansi(const std::wstring& wstr) {
 	auto count = WideCharToMultiByte(CODE_PAGE, 0, wstr.c_str(), static_cast<int>(wstr.length()), NULL, 0, NULL, NULL);
-	std::string str(count, 0);
+	auto str = std::string(count, 0);
 	WideCharToMultiByte(CODE_PAGE, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
 	return str;
 }
 
 std::string wide_to_utf8(const std::wstring& wstr) {
 	auto count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), static_cast<int>(wstr.length()), NULL, 0, NULL, NULL);
-	std::string str(count, 0);
+	auto str = std::string(count, 0);
 	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], static_cast<int>(count), NULL, NULL);
 	return str;
 }
 
 std::wstring ansi_to_wide(const std::string& str) {
 	auto count = MultiByteToWideChar(CODE_PAGE, 0, str.c_str(), static_cast<int>(str.length()), NULL, 0);
-	std::wstring wstr(count, 0);
+	auto wstr = std::wstring(count, 0);
 	MultiByteToWideChar(CODE_PAGE, 0, str.c_str(), static_cast<int>(str.length()), &wstr[0], count);
 	return wstr;
 }
 
 std::wstring utf8_to_wide(const std::string& str) {
 	auto count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), static_cast<int>(str.length()), NULL, 0);
-	std::wstring wstr(count, 0);
+	auto wstr = std::wstring(count, 0);
 	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), static_cast<int>(str.length()), &wstr[0], count);
 	return wstr;
 }
